2f5c82efb9213084fc0d81d5862644dade74422a
Arthur Martins <109447711+martins8@users.noreply.github.com>
2022-07-28 04:47:00 -0300

    Add files via upload

---
+++ manipulacaodeString.py
@@ -0,0 +1,14 @@
+# Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+# trecho de código 1
+string = "Arthur Martins de Lima"
+print(f'String: "{string}"\nO tamanho da string é: {len(string)}\n')
+
+# trecho de código 2
+string = "Estou realizando o curso de formação acelerada em programação front-end " \
+         "disponibilizado pela Softex Pernmabuco."
+print(f'String: "{string}"\nO número de vezes que o caracter "a" aparece na string é: {string.count("a")}\n')
+
+# trecho de código 2
+string = "Abacaxi"
+print(f'String: "{string}"\nAbacaxi ao contrário é: {string[::-1]}\n')
